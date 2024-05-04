a7a7c5e4b913a6690f428813314a4e421913fb7f
Aaron <reruaarr@gmail.com>
2024-05-03 20:58:49 -0600

    Muchisisisimos cambios pal login

--- views/user/Login.jsx
+++ views/user/Login.jsx
@@ -2,115 +2,168 @@ import React, { useState } from "react";
 import { View, Text, StyleSheet, Image, SafeAreaView } from "react-native";
 import { Input, Icon, Button } from "@rneui/themed";
 import { KeyboardAwareScrollView } from "react-native-keyboard-aware-scrollview";
-import Logo from "../../assets/Logo.png";
+import Logo from "../../assets/BlackLogo.png";
 
 export default function Login() {
   const [visiblePassword, setVisiblePassword] = useState(false);
+  const [keyboardActive, setKeyboardActive] = useState(false);
+
+  const handleKeyboardDidShow = () => {
+    setKeyboardActive(true);
+  };
+
+  const handleKeyboardDidHide = () => {
+    setKeyboardActive(false);
+  };
+
   return (
-    <SafeAreaView style={styles.areaStyle}>
-      <KeyboardAwareScrollView contentContainerStyle={styles.keyboarAware}>
+    <SafeAreaView style={styles.container}>
+      <KeyboardAwareScrollView
+        contentContainerStyle={[
+          styles.scrollViewContent,
+          keyboardActive && styles.keyboardActive,
+        ]}
+        extraScrollHeight={20}
+        keyboardShouldPersistTaps="handled"
+        onKeyboardDidShow={handleKeyboardDidShow}
+        onKeyboardDidHide={handleKeyboardDidHide}
+      >
         <View style={styles.title}>
-          <Image
-            source={Logo}
-            style={styles.imageLogo}
-          />
-          <Text style={styles.titletext}>- Car Repair Service -</Text>
+          <Image source={Logo} style={styles.imageLogo} />
+          <Text style={styles.titleText}>"Car Repair Service"</Text>
         </View>
+
         <View style={styles.form}>
-          <Text style={styles.labelForm}>Email</Text>
-          <Input inputStyle={styles.inputContainer} style={styles.inputForm} rightIcon={
+          <Text style={styles.label}>Email</Text>
+          <View style={styles.inputContainer}>
+            <Input
+              inputStyle={styles.input}
+              inputContainerStyle={{ borderBottomWidth: 0 }}
+            />
             <Icon
               name="email"
               type="material-community"
-              iconStyle={styles.inputIcon}
+              iconStyle={styles.icon}
             />
-          }/>
-          <Text style={styles.labelForm}>Password</Text>
-          <Input inputStyle={styles.inputContainer} style={styles.inputForm}
-            secureTextEntry={!visiblePassword}
-            rightIcon={
-              <Icon
-                name={ visiblePassword ? 'eye-off-outline' : 'eye-outline'}
-                type="material-community"
-                onPress={()=>setVisiblePassword(prev => !prev)}
-                iconStyle={styles.inputIcon}
-              />
-            }
-          />
-          <Button title={'SIGN IN'} 
-            buttonStyle={styles.btnForm}
-            containerStyle={styles.btnContainer}
-            titleStyle={styles.titleBtn}
-          />
+          </View>
+
+          <Text style={styles.label}>Password</Text>
+          <View style={styles.inputContainer}>
+            <Input
+              inputStyle={styles.input}
+              secureTextEntry={!visiblePassword}
+              inputContainerStyle={{ borderBottomWidth: 0 }}
+            />
+            <Icon
+              name={visiblePassword ? "eye-off-outline" : "eye-outline"}
+              type="material-community"
+              onPress={() => setVisiblePassword((prev) => !prev)}
+              iconStyle={styles.icon}
+            />
+          </View>
         </View>
-        <View>
-          <Text style={styles.signUP}>Dont have an account<Text style={{textDecorationLine: 'underline', color: '#FF7A00'}}> register now</Text></Text>
+
+        <Button
+          title="SIGN IN"
+          buttonStyle={styles.button}
+          containerStyle={styles.buttonContainer}
+          titleStyle={styles.buttonTitle}
+        />
+
+        <View style={styles.registerContainer}>
+          <Text style={styles.registerText}>
+            <Text style={styles.registerLink}>Register now</Text>
+          </Text>
         </View>
       </KeyboardAwareScrollView>
     </SafeAreaView>
   );
 }
 
-const styles = new StyleSheet.create({
-  areaStyle: {
+const styles = StyleSheet.create({
+  container: {
     flex: 1,
-  },
-  keyboarAware: {
-    flex: 1,
-    alignItems: 'center', 
     backgroundColor: "#E6961D",
   },
-  title: {
-    width: '100%',
-    justifyContent: "center",
+  scrollViewContent: {
+    flexGrow: 1,
     alignItems: "center",
+    justifyContent: "center",
   },
-  imageLogo:{
-    width: '60%',
-    height: '45%',
-    marginBottom: 2
+  keyboardActive: {
+    justifyContent: "flex-start",
   },
-  titletext:{
-    textAlign: 'center',
-    width: '100%',
-    color: '#0e0e0e',
-    fontSize: 20,
-    fontStyle: 'italic',
-    margin: 5
+  title: {
+    alignItems: "center",
+    marginBottom: 30,
   },
-  form:{
-    width: '80%',
-    justifyContent: 'center',
-    alignContent:'center',
-    alignItems: 'center'
+  imageLogo: {
+    width: 200,
+    height: 200,
+    resizeMode: "contain",
   },
-  inputForm:{
-    width: '70%',
-    color: '#0e0e0e',
+  titleText: {
+    color: "#0e0e0e",
+    fontSize: 24,
+    marginTop: 10,
   },
-  inputIcon:{
-    color: '#000'
+  form: {
+    width: "80%",
+    marginBottom: 20,
   },
-  inputContainer:{
-    backgroundColor: 'white',
-    borderRadius: 10
+  label: {
+    color: "#0e0e0e",
+    marginBottom: 5,
+    paddingLeft: 10,
+    paddingBottom: 5,
+    fontSize: 18,
+    fontWeight: "bold",
   },
-  labelForm: {
-    color: '#0e0e0e'
+  inputContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'space-between',
+    backgroundColor: '#fff',
+    height: 55,
+    borderRadius: 25,
+    paddingTop: 5, // reduce padding top
+    paddingBottom: 5, // reduce padding bottom
+    paddingRight: 40, // add padding right
+    marginBottom: 30,
   },
-  btnForm:{
-    backgroundColor:'#0e0e0e'
+  icon: {
+    color: "#000",
+    paddingRight: 10,
   },
-  btnContainer: {
-    width: '50%',
-    borderRadius: 8,
-    marginBottom: '5%'
+  input: {
+    flex: 1,
+    marginTop: 25,
+    paddingLeft: 5
   },
-  titleBtn:{
-    color: '#fff',
-    fontWeight: 'bold'
+  button: {
+    backgroundColor: "#0e0e0e",
+    borderRadius: 25,
+    paddingHorizontal: 40,
+    paddingVertical: 10,
   },
-  signUP:{
-    color: '#0e0e0e'
-  }
+  buttonContainer: {
+    marginBottom: 20,
+  },
+  buttonTitle: {
+    color: "#fff",
+    fontWeight: "bold",
+    fontSize: 18,
+  },
+  registerContainer: {
+    alignSelf: "flex-end",
+    paddingRight: "13%",
+    paddingTop: 5,
+  },
+  registerText: {
+    color: "#0e0e0e",
+    fontSize: 16
+  },
+  registerLink: {
+    color: "#0426FF",
+  },
 });
